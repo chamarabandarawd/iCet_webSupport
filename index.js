b0613d31f2c72286da6112c97e82940e117fe83c
chamarabandarawd <chamarabandarawd@gmail.com>
2023-06-09 15:17:23 +0530

    get current location in js

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const btn=$(".btn");
 const txt=$("#location");
 const temp=$("#temp");
 
+let lati=null;
+let longi=null;
+
+navigator.geolocation.getCurrentPosition((potition)=>{
+    console.log(potition);
+  lati=potition.coords.latitude;
+  longi=potition.coords.longitude;
+})
+
 btn.click(function(){
     console.log("btn clicked");
     let location=txt.val();
     fetch(
-        `http://api.weatherapi.com/v1/current.json?key=0deab009cb7a417381c155910231405&q=${location}`,
+        `http://api.weatherapi.com/v1/current.json?key=0deab009cb7a417381c155910231405&q=${lati},${longi}`,
         {
             method:"GET",
             mode:"cors"
