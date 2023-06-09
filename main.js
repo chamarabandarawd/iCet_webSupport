ba324919faea2c5ac729bd68562e0fc708332b66
chamarabandarawd <chamarabandarawd@gmail.com>
2023-06-09 11:36:02 +0530

    check request statats

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
+const para= document.getElementById("para");
 function btnClick(){
     console.log("Button clicked")
     const req=new XMLHttpRequest();
     console.log(req.readyState);
-    req.open("GET","DB/data.json");
+    req.open("GET","DB/db.txt");
     console.log(req.readyState);
     req.send();
     console.log(req.responseText);
     req.onreadystatechange=function(){
+        console.log(req.status);
         if(this.readyState==4){
-            console.log(req.responseText)
+            console.log(req.responseText);
+            para.innerText=req.responseText;
         }
     }
+  
 }
+
