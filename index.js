58e01e7a4c00b1e552aac283ec45ab6cf1cc281e
chamarabandarawd <chamarabandarawd@gmail.com>
2023-06-09 15:28:44 +0530

    runEveryMinute function

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ const temp=$("#temp");
 let lati=null;
 let longi=null;
 
-navigator.geolocation.getCurrentPosition((potition)=>{
-    console.log(potition);
-  lati=potition.coords.latitude;
-  longi=potition.coords.longitude;
-})
-
-btn.click(function(){
-    console.log("btn clicked");
+function runEveryMinute() {
+    navigator.geolocation.getCurrentPosition((potition)=>{
+        console.log(potition);
+      lati=potition.coords.latitude;
+      longi=potition.coords.longitude;
+    })
+    console.log("Function running every minute");
     let location=txt.val();
     fetch(
         `http://api.weatherapi.com/v1/current.json?key=0deab009cb7a417381c155910231405&q=${lati},${longi}`,
@@ -30,17 +29,6 @@ btn.click(function(){
         para.text(data.location.name);
        temp.text(data.current.temp_c);
     })
-
-});
-
-// $.ajax({
-//     url:`http://api.weatherapi.com/v1/current.json?key=0deab009cb7a417381c155910231405&q=${location}`,
-//     method:"GET",
-//     success:function (res){
-//       console.log(res);
-//       para.text(res.location.name);
-//       temp.text(res.current.temp_c);
-//     },
-//     error:function(er){
-//     }
-//   });
+}
+runEveryMinute();
+setInterval(runEveryMinute, 6000);
