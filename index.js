d150ca94b25214f2011638f173c8eb814aebaf37
chamarabandarawd <chamarabandarawd@gmail.com>
2023-06-09 14:34:20 +0530

    get data and display using ajax

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const para=$("#para");
 const btn=$(".btn");
+const txt=$("#location");
+const temp=$("#temp");
 
 btn.click(function(){
     console.log("btn clicked");
+    let location=txt.val();
     $.ajax({
-      url:"DB/db.txt",
-      method:"GET"
+      url:`http://api.weatherapi.com/v1/current.json?key=0deab009cb7a417381c155910231405&q=${location}`,
+      method:"GET",
+      success:function (res){
+        console.log(res);
+        para.text(res.location.name);
+        temp.text(res.current.temp_c);
+      },
+      error:function(er){
+      }
     });
 });
