bbbbd72e3dea501b72a598103a2004916ba00e19
chamarabandarawd <chamarabandarawd@gmail.com>
2023-06-09 14:57:18 +0530

    start fetch

--- index.js
+++ index.js
@@ -6,15 +6,30 @@ const temp=$("#temp");
 btn.click(function(){
     console.log("btn clicked");
     let location=txt.val();
-    $.ajax({
-      url:`http://api.weatherapi.com/v1/current.json?key=0deab009cb7a417381c155910231405&q=${location}`,
-      method:"GET",
-      success:function (res){
-        console.log(res);
-        para.text(res.location.name);
-        temp.text(res.current.temp_c);
-      },
-      error:function(er){
-      }
-    });
+    fetch(
+        `http://api.weatherapi.com/v1/current.json?key=0deab009cb7a417381c155910231405&q=${location}`,
+        {
+            method:"GET",
+            mode:"cors"
+        }
+    ).then(
+        (res)=>{
+          return  res.json();
+        }
+    ).then((data)=>{
+        console.log(data);
+    })
+
 });
+
+// $.ajax({
+//     url:`http://api.weatherapi.com/v1/current.json?key=0deab009cb7a417381c155910231405&q=${location}`,
+//     method:"GET",
+//     success:function (res){
+//       console.log(res);
+//       para.text(res.location.name);
+//       temp.text(res.current.temp_c);
+//     },
+//     error:function(er){
+//     }
+//   });
